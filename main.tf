5de60295bbdf233e7e80789b1097d4a69852490d
Evelina Andonova <69080997+evandonova@users.noreply.github.com>
2023-07-17 16:59:54 +0300

    Added backend block in Terraform configuration for state file

--- main.tf
+++ main.tf
@@ -5,6 +5,13 @@ terraform {
       version = "3.6.0"
     }
   }
+
+  backend "azurerm" {
+    resource_group_name  = "TaskBoardRG"
+    storage_account_name = "taskboardstorageacc"
+    container_name       = "taskboardcontainer"
+    key                  = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
